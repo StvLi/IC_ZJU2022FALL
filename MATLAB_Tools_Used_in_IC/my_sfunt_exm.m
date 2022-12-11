b84ec908de76e2b28b4916307ff7122720f4c308
Li Peize <3190102290@zju.edu.cn>
2022-12-11 22:11:26 +0800

    将智能控制作业代码整理进一个文件夹

---
+++ MATLAB_Tools_Used_in_IC/my_sfunt_exm.m
@@ -0,0 +1,158 @@
+function [sys,x0,str,ts,simStateCompliance] = my_sfunt_exm(t,x,u,flag)
+%SFUNTMPL General MATLAB S-Function Template
+%   Copyright 1990-2010 The MathWorks, Inc.
+
+%
+% The following outlines the general structure of an S-function.
+%
+switch flag,
+
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0,
+    [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes;
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1,
+    sys=mdlDerivatives(t,x,u);
+
+  %%%%%%%%%%
+  % Update %
+  %%%%%%%%%%
+  case 2,
+    sys=mdlUpdate(t,x,u);
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3,
+    sys=mdlOutputs(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%%%%
+  % GetTimeOfNextVarHit %
+  %%%%%%%%%%%%%%%%%%%%%%%
+  case 4,
+    sys=mdlGetTimeOfNextVarHit(t,x,u);
+
+  %%%%%%%%%%%%%
+  % Terminate %
+  %%%%%%%%%%%%%
+  case 9,
+    sys=mdlTerminate(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
+
+% end sfuntmpl
+
+%
+%=============================================================================
+% mdlInitializeSizes
+% Return the sizes, initial conditions, and sample times for the S-function.
+%=============================================================================
+%
+function [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes
+%
+% call simsizes for a sizes structure, fill it in and convert it to a
+% sizes array.
+%
+sizes = simsizes;
+sizes.NumContStates  = 2;   %   number of continuous state coordinates
+sizes.NumDiscStates  = 0;   %   number of discrete state coordinates
+sizes.NumOutputs     = 1;   %   number of Outputs
+sizes.NumInputs      = 1;   %   number of Inputs
+sizes.DirFeedthrough = 1;   %   throughout Feed (have no idea wtf it is)
+sizes.NumSampleTimes = 1;   %   at least one sample time is needed
+sys = simsizes(sizes);
+
+% initialize the initial conditions
+x0  = [ 0 0 ];
+
+% str is always an empty matrix
+str = [];
+
+% initialize the array of sample times
+ts  = [0 0];    %   continuous system
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+simStateCompliance = 'UnknownSimState';
+% end mdlInitializeSizes
+
+%
+%=============================================================================
+% mdlDerivatives
+% Return the derivatives for the continuous states.
+%=============================================================================
+%
+function sys=mdlDerivatives(t,x,u)
+A = [ 0 1 ; -1/80 -3/10];
+B = [ 0 3/16 ]';
+sys = A*x + B*u ;
+
+% end mdlDerivatives
+
+%
+%=============================================================================
+% mdlUpdate
+% Handle discrete state updates, sample time hits, and major time step
+% requirements.
+%=============================================================================
+%
+function sys=mdlUpdate(t,x,u)
+
+sys = [];
+
+% end mdlUpdate
+
+%
+%=============================================================================
+% mdlOutputs
+% Return the block outputs.
+%=============================================================================
+%
+function sys=mdlOutputs(t,x,u)
+C = [ 1 0 ];
+D = 0;
+sys = C*x + D*u ;
+
+% end mdlOutputs
+
+%
+%=============================================================================
+% mdlGetTimeOfNextVarHit
+% Return the time of the next hit for this block.  Note that the result is
+% absolute time.  Note that this function is only used when you specify a
+% variable discrete-time sample time [-2 0] in the sample time array in
+% mdlInitializeSizes.
+%=============================================================================
+%
+function sys=mdlGetTimeOfNextVarHit(t,x,u)
+
+sampleTime = 1;    %  Example, set the next hit to be one second later.
+sys = t + sampleTime;
+
+% end mdlGetTimeOfNextVarHit
+
+%
+%=============================================================================
+% mdlTerminate
+% Perform any end of simulation tasks.
+%=============================================================================
+%
+function sys=mdlTerminate(t,x,u)
+
+sys = [];
+
+% end mdlTerminate
